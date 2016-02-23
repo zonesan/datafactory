41db94632819b3e9ab568fd5d119f21644ad727f
michael pan <1851616111@qq.com>
2016-02-23 11:42:18 +0800

    [bugfix]servicebroker客户端参数检查 10m

--- pkg/cmd/cli/cmd/servicebroker.go
+++ pkg/cmd/cli/cmd/servicebroker.go
@@ -43,9 +43,9 @@ func NewCmdServiceBroker(fullName string, f *clientcmd.Factory, out io.Writer) *
 		Example: fmt.Sprintf(newServiceBrokerExample, fullName),
 		Run: func(cmd *cobra.Command, args []string) {
 			var err error
-
 			if options.complete(cmd, f); err != nil {
 				kcmdutil.CheckErr(err)
+				return
 			}
 
 			if options.Client, _, err = f.Clients(); err != nil {
