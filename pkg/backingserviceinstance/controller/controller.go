ae3bfc0800de5a8ee021e114f0a03b21eb277929
zonesan <san@zoobook.org>
2016-02-23 16:41:44 +0800

    Create Service instance via sericebroker. PUT /v2/service_instances/:id to create Service instance.

--- pkg/backingserviceinstance/controller/controller.go
+++ pkg/backingserviceinstance/controller/controller.go
@@ -3,13 +3,15 @@ package controller
 import (
 	backingserviceapi "github.com/openshift/origin/pkg/backingservice/api"
 
-	backingserviceinstanceapi "github.com/openshift/origin/pkg/backingserviceinstance/api"
-	osclient "github.com/openshift/origin/pkg/client"
-	kclient "k8s.io/kubernetes/pkg/client/unversioned"
-
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/golang/glog"
+	backingserviceinstanceapi "github.com/openshift/origin/pkg/backingserviceinstance/api"
+	osclient "github.com/openshift/origin/pkg/client"
+	"io/ioutil"
+	kclient "k8s.io/kubernetes/pkg/client/unversioned"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/util"
@@ -34,11 +36,10 @@ func (e fatalError) Error() string {
 
 // Handle processes a namespace and deletes content in origin if its terminating
 func (c *BackingServiceInstanceController) Handle(bsi *backingserviceinstanceapi.BackingServiceInstance) (err error) {
-	fmt.Println("bsi handler called.")
+	glog.Infoln("bsi handler called.", bsi.Name)
 	if bsi.Status.Phase == backingserviceinstanceapi.BackingServiceInstancePhaseReady {
 		return nil
 	}
-
 
 	if ok, bs, err := checkIfPlanidExist(c.Client, bsi.Spec.BackingServicePlanGuid); !ok {
 		if bsi.Status.Phase != backingserviceinstanceapi.BackingServiceInstancePhaseError {
@@ -51,51 +52,47 @@ func (c *BackingServiceInstanceController) Handle(bsi *backingserviceinstanceapi
 		bsi.Spec.BackingServiceName = bs.ObjectMeta.Name
 	}
 
+	sb, err := c.Client.ServiceBrokers().Get(bsi.Spec.BackingServiceName)
+	if err != nil {
+		return err
+	}
+
 	bsInstanceID := string(util.NewUUID())
 	bsi.Status.Phase = backingserviceinstanceapi.BackingServiceInstancePhaseReady
 	bsi.Spec.Parameters = make(map[string]string)
 	bsi.Spec.Parameters["instance_id"] = bsInstanceID
 
-	type sbsvcinstance struct {
-		ServiceId        string `json:"service_id"`
-		PlanId           string `json:"plan_id"`
-		OrganizationGuid string `json:"organization_guid"`
-		SpaceGuid        string `json:"space_guid"`
-		//Incomplete       bool        `json:"accepts_incomplete, omitempty"`
-		Parameters interface{} `json:"parameters, omitempty"`
+	sbi := &SBServiceInstance{}
+	sbi.ServiceId = bsInstanceID
+	sbi.PlanId = bsi.Spec.BackingServicePlanGuid
+	sbi.OrganizationGuid = bsi.Namespace
+
+	if svcinstance, err := servicebroker_create_instance(sbi, bsInstanceID, sb.Spec.Url, sb.Spec.UserName, sb.Spec.Password); err != nil {
+		glog.Errorln(err)
+
+	} else {
+
+		glog.Infoln("create instance successfully.", svcinstance)
 	}
 
-	sbInstanceUrl := "http://$ServiceBrokerUrl" + "/v2/service_instances/" + bsInstanceID
-	fmt.Println(sbInstanceUrl)
-
-	jsonData, err := json.Marshal(sbsvcinstance{})
-	if err != nil {
-		return err
-	}
-	resp, err := commToServiceBroker("PUT", sbInstanceUrl, jsonData, nil)
-	if err != nil {
-		//glog.Error(err)
-		fmt.Println(err)
-		return err
-	}
-	defer resp.Body.Close()
-
-
-	c.Client.BackingServiceInstances().Update(bsi)
 	/*
-		if bsi.Status.Phase != backingserviceinstanceapi.BackingServiceInstancePhaseActive {
-			bsi.Status.Phase = backingserviceinstanceapi.BackingServiceInstancePhaseActive
-
-			c.Client.BackingServiceInstances().Update(bsi)
+		resp, err := commToServiceBroker("PUT", sbInstanceUrl, jsonData, nil)
+		if err != nil {
+			//glog.Error(err)
+			fmt.Println(err)
+			return err
 		}
+		defer resp.Body.Close()
 	*/
 
+	c.Client.BackingServiceInstances().Update(bsi)
 	return nil
 }
 
 func checkIfPlanidExist(client osclient.Interface, planId string) (bool, *backingserviceapi.BackingService, error) {
 
 	items, err := client.BackingServices().List(labels.Everything(), fields.Everything())
+
 	if err != nil {
 		return false, nil, err
 	}
@@ -103,15 +100,15 @@ func checkIfPlanidExist(client osclient.Interface, planId string) (bool, *backin
 	for _, bs := range items.Items {
 		for _, plans := range bs.Spec.Plans {
 			if planId == plans.Id {
-				fmt.Println("we found plan id at plan", bs.Spec.Name)
+				glog.Info("we found plan id at plan", bs.Spec.Name)
 
 				return true, &bs, nil
 			}
 		}
 	}
 	return false, nil, fatalError(fmt.Sprintf("Can't find plan id %s", planId))
-}
 
+}
 
 func commToServiceBroker(method, path string, jsonData []byte, header map[string]string) (resp *http.Response, err error) {
 
@@ -128,3 +125,70 @@ func commToServiceBroker(method, path string, jsonData []byte, header map[string
 	return http.DefaultClient.Do(req)
 }
 
+type SBServiceInstance struct {
+	ServiceId        string `json:"service_id"`
+	PlanId           string `json:"plan_id"`
+	OrganizationGuid string `json:"organization_guid"`
+	SpaceGuid        string `json:"space_guid"`
+	//Incomplete       bool        `json:"accepts_incomplete, omitempty"`
+	Parameters interface{} `json:"parameters, omitempty"`
+}
+type LastOperation struct {
+	State                    string `json:"state"`
+	Description              string `json:"description"`
+	AsyncPollIntervalSeconds int    `json:"async_poll_interval_seconds, omitempty"`
+}
+type CreateServiceInstanceResponse struct {
+	DashboardUrl  string         `json:"dashboard_url"`
+	LastOperation *LastOperation `json:"last_operation, omitempty"`
+}
+
+func servicebroker_create_instance(param *SBServiceInstance, instance_guid, broker_url, username, password string) (*CreateServiceInstanceResponse, error) {
+	jsonData, err := json.Marshal(param)
+	if err != nil {
+		return nil, err
+	}
+
+	header := make(map[string]string)
+	header["Content-Type"] = "application/json"
+	header["Authorization"] = basicAuthStr(username, password)
+
+	resp, err := commToServiceBroker("PUT", broker_url+"/v2/service_instances/"+instance_guid, jsonData, header)
+	if err != nil {
+
+		glog.Error(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	glog.Infof("respcode from /v2/service_instances/%s: %v", instance_guid, resp.StatusCode)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		glog.Error(err)
+		return nil, err
+	}
+	svcinstance := &CreateServiceInstanceResponse{}
+
+	glog.Infof("%v,%+v\n", string(body), svcinstance)
+	if resp.StatusCode == http.StatusOK {
+		if len(body) > 0 {
+			err = json.Unmarshal(body, svcinstance)
+
+			if err != nil {
+				fmt.Println(err)
+				return nil, err
+			}
+		}
+	}
+
+	glog.Info("%v,%+v\n", string(body), svcinstance)
+
+	return svcinstance, nil
+}
+
+func basicAuthStr(username, password string) string {
+	auth := username + ":" + password
+	authstr := base64.StdEncoding.EncodeToString([]byte(auth))
+	return "Basic " + authstr
+}
