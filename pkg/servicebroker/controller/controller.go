70a20d02175b134ef420a3215cdd810f7109e47b
michael pan <1851616111@qq.com>
2016-03-14 21:24:29 +0800

    [fix bug] 增加servicebroker 新建和运行态的ping功能

--- pkg/servicebroker/controller/controller.go
+++ pkg/servicebroker/controller/controller.go
@@ -5,10 +5,16 @@ import (
 
 	osclient "github.com/openshift/origin/pkg/client"
 	servicebrokerapi "github.com/openshift/origin/pkg/servicebroker/api"
-
+	backingserviceapi "github.com/openshift/origin/pkg/backingservice/api"
 	"github.com/golang/glog"
 	backingservice "github.com/openshift/origin/pkg/backingservice/api"
 	servicebrokerclient "github.com/openshift/origin/pkg/servicebroker/client"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/api/errors"
+	"k8s.io/kubernetes/pkg/fields"
+	"time"
+	"fmt"
+	"strconv"
 )
 
 // NamespaceController is responsible for participating in Kubernetes Namespace termination
@@ -35,45 +41,159 @@ func (c *ServiceBrokerController) Handle(sb *servicebrokerapi.ServiceBroker) (er
 		return nil
 	}
 
-	services, err := c.ServiceBrokerClient.Catalog(sb.Spec.Url, sb.Spec.UserName, sb.Spec.Password)
-	if err != nil {
-		glog.Infof("ServiceBroker %s catalog err %s\n", sb.Name, err.Error())
-		//time.Sleep(time.Minute * 5)
-		if sb.Status.Phase != servicebrokerapi.ServiceBrokerFailed {
+	switch sb.Status.Phase {
+	case servicebrokerapi.ServiceBrokerNew:
+		if getRetryTime(sb) <= 3 {
+			if Ping(sb, 10) {
+				setRetryTime(sb)
+
+				services, err := c.ServiceBrokerClient.Catalog(sb.Spec.Url, sb.Spec.UserName, sb.Spec.Password)
+				if err != nil {
+					return err
+				}
+
+				errs := []error{}
+				for _, v := range services.Services {
+					backingService := &backingservice.BackingService{}
+					backingService.Spec = backingservice.BackingServiceSpec(v)
+					backingService.Annotations = make(map[string]string)
+					backingService.Name = v.Name
+					backingService.GenerateName = sb.Name
+					backingService.Labels = map[string]string{
+						servicebrokerapi.ServiceBrokerLabel: sb.Name,
+					}
+
+					_, err := c.Client.BackingServices().Get(backingService.Name)
+					if err != nil {
+						if errors.IsNotFound(err) {
+							if _, err := c.Client.BackingServices().Create(backingService); err != nil {
+								glog.Errorln("servicebroker create backingservice err ", err)
+								errs = append(errs, err)
+							}
+						}
+					} else {
+						if _, err := c.Client.BackingServices().Update(backingService); err != nil {
+							glog.Errorln("servicebroker update backingservice err ", err)
+							errs = append(errs, err)
+						}
+					}
+				}
+
+				if len(errs) == 0 {
+					removeRetryTime(sb)
+					sb.Status.Phase = servicebrokerapi.ServiceBrokerActive
+				}
+
+				c.Client.ServiceBrokers().Update(sb)
+				return nil
+			}
+		} else {
 			sb.Status.Phase = servicebrokerapi.ServiceBrokerFailed
 			c.Client.ServiceBrokers().Update(sb)
+
+			c.inActiveBackingService(sb.Name)
+			return nil
 		}
 
+	case servicebrokerapi.ServiceBrokerDeleting:
+		c.inActiveBackingService(sb.Name)
+		c.Client.ServiceBrokers().Delete(sb.Name)
 		return nil
-	}
+	case servicebrokerapi.ServiceBrokerActive:
+		if Ping(sb, 60) {
+			_, err := c.ServiceBrokerClient.Catalog(sb.Spec.Url, sb.Spec.UserName, sb.Spec.Password)
+			if err != nil {
+				sb.Status.Phase = servicebrokerapi.ServiceBrokerFailed
+				c.Client.ServiceBrokers().Update(sb)
+				return err
+			}
 
-	finish := false
-	for _, v := range services.Services {
-		backingService := &backingservice.BackingService{}
-		backingService.Spec = backingservice.BackingServiceSpec(v)
-		backingService.Annotations = make(map[string]string)
-		backingService.Name = v.Name
-		backingService.GenerateName = sb.Name
-		backingService.Labels = map[string]string{
-			servicebrokerapi.ServiceBrokerLabel: sb.Name,
+			c.Client.ServiceBrokers().Update(sb)
+			return nil
 		}
-		glog.Info("create backingservice")
-		if _, err := c.Client.BackingServices().Create(backingService); err == nil {
-			glog.Info("create backingservice successfuly!", backingService)
-			finish = true
-		} else {
-			glog.Info(err)
-			return err
-		}
-	}
-	if finish == true {
-		//TODO
-	}
+	case servicebrokerapi.ServiceBrokerFailed:
+		if Ping(sb, 60) {
+			_, err := c.ServiceBrokerClient.Catalog(sb.Spec.Url, sb.Spec.UserName, sb.Spec.Password)
+			if err != nil {
+				c.Client.ServiceBrokers().Update(sb)
+				return err
+			}
 
-	if sb.Status.Phase != servicebrokerapi.ServiceBrokerActive {
-		sb.Status.Phase = servicebrokerapi.ServiceBrokerActive
-		c.Client.ServiceBrokers().Update(sb)
+			sb.Status.Phase = servicebrokerapi.ServiceBrokerActive
+			c.Client.ServiceBrokers().Update(sb)
+			return nil
+		}
+
 	}
 
 	return nil
+}
+
+func (c *ServiceBrokerController) inActiveBackingService(serviceBrokerName string) {
+	selector, _ := labels.Parse(servicebrokerapi.ServiceBrokerLabel + "=" + serviceBrokerName)
+
+	bsList, err := c.Client.BackingServices().List(selector, fields.Everything())
+	if err == nil {
+		for _, bsvc := range bsList.Items {
+			bsvc.Status.Phase = backingserviceapi.BackingServicePhaseInactive
+			c.Client.BackingServices().Update(&bsvc)
+		}
+	}
+}
+
+func Ping(sb *servicebrokerapi.ServiceBroker, pingSecond int64) bool {
+	lastTimeStr := sb.Annotations[servicebrokerapi.ServiceBrokerLastPingTime]
+	if len(lastTimeStr) == 0 {
+		sb.Annotations[servicebrokerapi.ServiceBrokerLastPingTime] = fmt.Sprintf("%d", time.Now().UnixNano())
+		return true
+	}
+
+	lastPing, err := strconv.Atoi(lastTimeStr)
+	if err != nil {
+		sb.Annotations[servicebrokerapi.ServiceBrokerLastPingTime] = fmt.Sprintf("%d", time.Now().UnixNano())
+		return false
+	}
+
+	if time.Now().UnixNano() - int64(lastPing) < pingSecond {
+		return false
+	}
+
+	sb.Annotations[servicebrokerapi.ServiceBrokerLastPingTime] = fmt.Sprintf("%d", time.Now().UnixNano())
+	return true
+}
+
+func getRetryTime(sb *servicebrokerapi.ServiceBroker) int {
+	retries := sb.Annotations[servicebrokerapi.ServiceBrokerNewRetryTimes]
+	if len(retries) == 0 {
+		return 0
+	}
+
+	i, err := strconv.Atoi(retries)
+	if err != nil {
+		return 0
+	}
+
+	return i
+}
+
+func setRetryTime(sb *servicebrokerapi.ServiceBroker) {
+	retries := sb.Annotations[servicebrokerapi.ServiceBrokerNewRetryTimes]
+	if len(retries) == 0 {
+		sb.Annotations[servicebrokerapi.ServiceBrokerNewRetryTimes] = fmt.Sprintf("%d", 1)
+		return
+	}
+
+	i, err := strconv.Atoi(retries)
+	if err != nil {
+		sb.Annotations[servicebrokerapi.ServiceBrokerNewRetryTimes] = fmt.Sprintf("%d", 1)
+		return
+	}
+
+	sb.Annotations[servicebrokerapi.ServiceBrokerNewRetryTimes] = fmt.Sprintf("%d", i + 1)
+
+	return
+}
+
+func removeRetryTime(sb *servicebrokerapi.ServiceBroker) {
+	delete(sb.Annotations, servicebrokerapi.ServiceBrokerNewRetryTimes)
 }
