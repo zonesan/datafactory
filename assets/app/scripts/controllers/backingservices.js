6539d057619bc37848f87248a5dd0c1f3b411143
zhenglipeng <1252012805@qq.com>
2016-03-17 16:13:46 +0800

    [bug fix]:backing service添加namespace（默认为openshift）

--- assets/app/scripts/controllers/backingservices.js
+++ assets/app/scripts/controllers/backingservices.js
@@ -8,19 +8,24 @@
  * Controller of the openshiftConsole
  */
 angular.module('openshiftConsole')
-  .controller('BackingservicesController', function ($scope, AuthService, DataService) {
+  .controller('BackingservicesController', function ($scope, $routeParams, AuthService, DataService, ProjectsService) {
 
     $scope.emptyMessage = '现在没有数据...';
 
-    AuthService.withUser().then(function() {
-      loadBackingServices();
+    var watches = [];
+
+    ProjectsService
+      .get($routeParams.project)
+      .then(_.spread(function(project, context) {
+        $scope.project = project;
+        watches.push(DataService.watch("backingservices", context, function(backingservices) {
+          $scope.backingservices = backingservices.by("metadata.name");
+          console.log("backingservices", $scope.backingservices);
+        }));
+      })
+    );
+
+    $scope.$on('$destroy', function(){
+      DataService.unwatchAll(watches);
     });
-
-    var loadBackingServices = function() {
-      DataService.list("backingservices", $scope, function(backingservices){
-        $scope.backingservices = backingservices.by("metadata.name");
-        console.log("backingservices", $scope.backingservices);
-      });
-    };
-
   });
